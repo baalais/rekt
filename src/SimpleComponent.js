bc6be474bb0b587ee721493e379869bbfd9d7f99
ipb21.t.rudzitis <ipb21.t.rudzitis@pjvt.local>
2023-10-11 14:48:32 +0300

    jaunais gejs

---
+++ src/SimpleComponent.js
@@ -0,0 +1,26 @@
+import './style.css';
+
+function SimpleComponent() {
+    return (
+    <>
+    <header>
+        <h1>🍎 Prasības un Prasību specifikācija 📋</h1>
+    </header>
+    <main>
+        <section>
+            <h2>Kas ir prasības?</h2>
+            <p>Prasības ir kā ķermeņa šūnas - nepieciešamas, lai projekts dzīvotu. Tās ir vispārēji noteikumi, kas nodrošina veiksmīgu gaitu.</p>
+        </section>
+        <section>
+            <h2>Kas ir prasību specifikācija?</h2>
+            <p>Prasību specifikācija ir kā recepte ābolu sēnēm - detalizēts dokuments, kas nosaka, kā ķermeņa šūnas darbosies. Tajā ir katra sastāvdaļa un process.</p>
+        </section>
+        <section>
+            <h2>Atšķirība starp prasībām un prasību specifikāciju</h2>
+            <p>Prasības ir saprātīgs garšīgums, bet prasību specifikācija ir īsta ābolu pīrāga recepte - detalizēta un skaidra, lai to varētu izgatavot ikviens.</p>
+        </section>
+    </main>
+    </>
+    );
+  }
+export default SimpleComponent;
